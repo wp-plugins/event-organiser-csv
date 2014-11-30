bb54d3bfc66c815d2b59c3d4d79816a5a031a781
stephenharris <stephenharris@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-30 20:21:47 +0000

    Tagging 0.2.0
    git-svn-id: https://plugins.svn.wordpress.org/event-organiser-csv/tags/0.2.0@1035609 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- event-organiser-csv.php
+++ event-organiser-csv.php
@@ -3,7 +3,7 @@
  * Plugin Name: Event Organiser CSV
  * Plugin URI:  http://wp-event-organiser.com/
  * Description: Import and Export events via CSV 
- * Version:     0.1.3
+ * Version:     0.2.0
  * Author:      Stephen Harris
  * Author URI:  http://stephenharris.info
  * License:     GPLv2+
@@ -34,7 +34,7 @@
  * and code has been used from that to kick-start this plug-in.
  */
 
-define( 'EVENT_ORGANISER_CSV_VERSION', '0.1.3' );
+define( 'EVENT_ORGANISER_CSV_VERSION', '0.2.0' );
 define( 'EVENT_ORGANISER_CSV_URL',     plugin_dir_url( __FILE__ ) );
 define( 'EVENT_ORGANISER_CSV_DIR',    dirname( __FILE__ ) . '/' );
 
@@ -47,6 +47,8 @@ function eventorganisercsv_init() {
 	
 	if( is_admin() ){
 		
+		require_once( EVENT_ORGANISER_CSV_DIR.'includes/class-eo-csv.php');
+		
 		require_once( EVENT_ORGANISER_CSV_DIR.'includes/class-eo-csv-parser.php');
 		require_once( EVENT_ORGANISER_CSV_DIR.'includes/class-eo-event-csv-parser.php');
 		require_once( EVENT_ORGANISER_CSV_DIR.'includes/admin.php');
@@ -58,25 +60,27 @@ function eventorganisercsv_init() {
 		wp_register_script( 'eo_csv_jquery_csv', EVENT_ORGANISER_CSV_URL . "assets/js/vendor/jquery-csv.js", array( 'jquery' ),  EVENT_ORGANISER_CSV_VERSION );
 		wp_register_script( 'eo_csv_admin', EVENT_ORGANISER_CSV_URL . "assets/js/event_organiser_csv{$ext}.js", array( 'jquery', 'eo_csv_jquery_csv' ),  EVENT_ORGANISER_CSV_VERSION );
 		wp_register_style( 'eo_csv_admin', EVENT_ORGANISER_CSV_URL . "assets/css/event_organiser_csv{$ext}.css", array(),  EVENT_ORGANISER_CSV_VERSION );
+
+		
+		$columns = 	apply_filters( 'eventorganiser_csv_import_columns', array(
+			'post_title'     => __( 'Title', 'event-organiser-csv' ),
+			'start'          => __( 'Start', 'event-organiser-csv' ),
+			'end'            => __( 'End', 'event-organiser-csv' ),
+			'schedule_last'  => __( 'Recur until', 'event-organiser-csv' ),
+			'schedule'       => __( 'Recurrence schedule', 'event-organiser-csv' ),
+			'frequency'      => __( 'Recurrence frequency', 'event-organiser-csv' ),
+			'schedule_meta'  => __( 'Schedule meta', 'event-organiser-csv' ),
+			'post_content'   => __( 'Content', 'event-organiser-csv' ),
+			'event-venue'    => __( 'Venue', 'event-organiser-csv' ),
+			'event-category' => __( 'Category', 'event-organiser-csv' ),
+			'event-tag'      => __( 'Tags', 'event-organiser-csv' ),
+			'include'        => __( 'Include dates', 'event-organiser-csv' ),
+			'exclude'        => __( 'Exclude dates', 'event-organiser-csv' ),
+			'post_meta'      => __( 'Post Meta', 'event-organiser-csv' ),
+		));
 		
 		wp_localize_script( 'eo_csv_admin', 'eo_csv', array(
-			'locale' => array(
-				'title' 			=> __( 'Title', 'event-organiser-csv' ),
-				'start' 			=> __( 'Start', 'event-organiser-csv' ),
-				'end' 				=> __( 'End', 'event-organiser-csv' ),
-				'recur_until' 		=> __( 'Recur until', 'event-organiser-csv' ),
-				'recur_schedule' 	=> __( 'Recurrence schedule', 'event-organiser-csv' ),
-				'recur_freq'	 	=> __( 'Recurrence frequency', 'event-organiser-csv' ),
-				'schedule_meta'		=> __( 'Schedule meta', 'event-organiser-csv' ),
-				'content' 			=> __( 'Content', 'event-organiser-csv' ),
-				'venue' 			=> __( 'Venue', 'event-organiser-csv' ),
-				'categories' 		=> __( 'Categories', 'event-organiser-csv' ),
-				'tags' 				=> __( 'Tags', 'event-organiser-csv' ),
-				'include_dates' 	=> __( 'Include dates', 'event-organiser-csv' ),
-				'exclude_dates' 	=> __( 'Exclude dates', 'event-organiser-csv' ),
-				'post_meta' 		=> __( 'Post Meta', 'event-organiser-csv' ),
-			)
-		
+			'columns' => $columns,		
 		));
 	}
 }
