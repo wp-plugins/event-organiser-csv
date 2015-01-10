1c3f5c8393464e9f9c42f35f230d8410ebe6ac0c
stephenharris <stephenharris@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-10 15:54:18 +0000

    Tagging 0.3.0
    git-svn-id: https://plugins.svn.wordpress.org/event-organiser-csv/trunk@1064717 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- event-organiser-csv.php
+++ event-organiser-csv.php
@@ -3,7 +3,7 @@
  * Plugin Name: Event Organiser CSV
  * Plugin URI:  http://wp-event-organiser.com/
  * Description: Import and Export events via CSV 
- * Version:     0.2.0
+ * Version:     0.3.0
  * Author:      Stephen Harris
  * Author URI:  http://stephenharris.info
  * License:     GPLv2+
@@ -34,7 +34,7 @@
  * and code has been used from that to kick-start this plug-in.
  */
 
-define( 'EVENT_ORGANISER_CSV_VERSION', '0.2.0' );
+define( 'EVENT_ORGANISER_CSV_VERSION', '0.3.0' );
 define( 'EVENT_ORGANISER_CSV_URL',     plugin_dir_url( __FILE__ ) );
 define( 'EVENT_ORGANISER_CSV_DIR',    dirname( __FILE__ ) . '/' );
 
@@ -61,8 +61,7 @@ function eventorganisercsv_init() {
 		wp_register_script( 'eo_csv_admin', EVENT_ORGANISER_CSV_URL . "assets/js/event_organiser_csv{$ext}.js", array( 'jquery', 'eo_csv_jquery_csv' ),  EVENT_ORGANISER_CSV_VERSION );
 		wp_register_style( 'eo_csv_admin', EVENT_ORGANISER_CSV_URL . "assets/css/event_organiser_csv{$ext}.css", array(),  EVENT_ORGANISER_CSV_VERSION );
 
-		
-		$columns = 	apply_filters( 'eventorganiser_csv_import_columns', array(
+		$columns = array(
 			'post_title'     => __( 'Title', 'event-organiser-csv' ),
 			'start'          => __( 'Start', 'event-organiser-csv' ),
 			'end'            => __( 'End', 'event-organiser-csv' ),
@@ -70,14 +69,29 @@ function eventorganisercsv_init() {
 			'schedule'       => __( 'Recurrence schedule', 'event-organiser-csv' ),
 			'frequency'      => __( 'Recurrence frequency', 'event-organiser-csv' ),
 			'schedule_meta'  => __( 'Schedule meta', 'event-organiser-csv' ),
-			'post_content'   => __( 'Content', 'event-organiser-csv' ),
+			'post_content'   => __( 'Content', 'event-organiser-csv' )
+		);
+		
+		//Taxonomies
+		$event_taxonomies = get_object_taxonomies( 'event', 'objects' );
+		if( $event_taxonomies ){
+			foreach( $event_taxonomies as $taxonomy ){
+				$columns[$taxonomy->name] = $taxonomy->label;
+			}
+		}
+
+		$columns = $columns + array(
 			'event-venue'    => __( 'Venue', 'event-organiser-csv' ),
 			'event-category' => __( 'Category', 'event-organiser-csv' ),
 			'event-tag'      => __( 'Tags', 'event-organiser-csv' ),
 			'include'        => __( 'Include dates', 'event-organiser-csv' ),
 			'exclude'        => __( 'Exclude dates', 'event-organiser-csv' ),
 			'post_meta'      => __( 'Post Meta', 'event-organiser-csv' ),
-		));
+		);
+		
+		
+		
+		$columns = 	apply_filters( 'eventorganiser_csv_import_columns', $columns );
 		
 		wp_localize_script( 'eo_csv_admin', 'eo_csv', array(
 			'columns' => $columns,		
